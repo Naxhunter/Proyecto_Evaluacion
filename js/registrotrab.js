cd6981f7d447ef83e0e0ec269a420cf33f837d1f
Naxhunter <jos.contrerasv@duocuc.cl>
2021-04-24 19:33:41 -0400

    creando las funciones de la validacion trabajador

--- js/registrotrab.js
+++ js/registrotrab.js
@@ -1,3 +1,28 @@
 function validar_trabajador(){
     
+        var validar = rut();
+        if(validar==false){
+            return false;
+        }
+        validar = email();
+        if(validar==false){
+            return false;
+        }
+        validar = nombre();
+        if(validar==false){
+            return false;
+        }
+        validar = contra();
+        if(validar==false){
+            return false;
+        }
+        validar = especialidad();
+        if(validar==false){
+            return false;
+        }
+        validar = telefono();
+        if(validar==false){
+            return false;
+        }
+        return true;
 }
